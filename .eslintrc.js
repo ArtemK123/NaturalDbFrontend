cbd339022f8424f2b7a59e461fb9470dd544a008
ArtemK123 <darkmode1012@gmail.com>
2022-11-02 08:51:23 +0200

    Added linter

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    "airbnb-typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "plugin:prettier/recommended"
+  ],
+  overrides: [
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: "module",
+    project: "./tsconfig.json",
+
+  },
+  plugins: ["react", "@typescript-eslint", "jest", "import"],
+  rules: {
+    "prettier/prettier": ["error", { endOfLine: "lf", singleQuote: false, printWidth: 120 }],
+  }
+}
