cfccfa0c2a38f95cd9f1e425cbea7618de2b74f6
ArtemK123 <darkmode1012@gmail.com>
2022-11-06 13:05:03 +0200

    Created draft of the state layout for the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import { Box, Button, styled, TextField, ThemeProvider } from "@mui/material";
+import { Box, Button, styled, TextField } from "@mui/material";
 import { useState } from "react";
 import { Colors } from "./colors";
-import { theme } from "./theme";
 
 function App() {
   const [textQuery, setTextQuery] = useState("");
@@ -39,37 +38,123 @@ function App() {
     setMediaRecorder(undefined);
   }
 
+  const [state, setState] = useState(States.NaturalLanguageQuery);
+  const [formalQuery, setFormalQuery] = useState<string>("");
+  const [queryResult, setQueryResult] = useState<string>("");
+
+  const naturalLanguageQueryViewCallback = (newFormalQuery: string) => {
+    setState(States.FormalQuery);
+    setFormalQuery(newFormalQuery);
+  };
+
+  const formalQueryViewCallback = (newQueryResult: string) => {
+    setState(States.QueryResult);
+    setQueryResult(newQueryResult);
+  };
+
   return (
-    <ThemeProvider theme={theme}>
-      <Main>
-        <img src="/dog.png" />
-        <Title></Title>
-        <Box sx={{ width: "500px", display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
-          {mediaRecorder ? (
-            <button onClick={stopRecording}>Stop</button>
-          ) : (
-            <button onClick={startRecording}>Record</button>
-          )}
-          <button>Translate to text</button>
-        </Box>
-        {audioBlob && <audio controls src={URL.createObjectURL(audioBlob)}></audio>}
-        <StyledForm onSubmit={onSubmit}>
-          <TextField
-            label="Input"
-            sx={{ marginY: "20px" }}
-            multiline
-            maxRows={4}
-            value={textQuery}
-            onChange={(e) => setTextQuery(e.target.value)}
-          />
-          <Button type="submit" variant="contained">
-            Send
-          </Button>
-        </StyledForm>
-        <div>{result}</div>
-      </Main>
-    </ThemeProvider>
+    <Main>
+      <img src="/dog.png" />
+      {state === States.NaturalLanguageQuery && (
+        <NaturalLanguageQueryView callback={naturalLanguageQueryViewCallback} />
+      )}
+      {state === States.FormalQuery && <FormalQueryView query={formalQuery} callback={formalQueryViewCallback} />}
+      {state === States.QueryResult && <QueryResultView result={queryResult} />}
+    </Main>
   );
+
+  // return (
+  //   <Main>
+  //     <img src="/dog.png" />
+  //     <Title>Natural Db</Title>
+  //     <Box sx={{ width: "500px", display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+  //       {mediaRecorder ? (
+  //         <button onClick={stopRecording}>Stop</button>
+  //       ) : (
+  //         <button onClick={startRecording}>Record</button>
+  //       )}
+  //       <button>Translate to text</button>
+  //     </Box>
+  //     {audioBlob && <audio controls src={URL.createObjectURL(audioBlob)}></audio>}
+  //     <StyledForm onSubmit={onSubmit}>
+  //       <TextField
+  //         label="Input"
+  //         sx={{ marginY: "20px" }}
+  //         multiline
+  //         maxRows={4}
+  //         value={textQuery}
+  //         onChange={(e) => setTextQuery(e.target.value)}
+  //       />
+  //       <Button type="submit" variant="contained">
+  //         Send
+  //       </Button>
+  //     </StyledForm>
+  //     <div>{result}</div>
+  //   </Main>
+  // );
+}
+
+function NaturalLanguageQueryView({ callback }: { callback: (formalQuery: string) => void }) {
+  const [inputType, setInputType] = useState(NaturalLanguageInputTypes.Text);
+
+  return (
+    <>
+      <Title>Provide your command</Title>
+      <div>
+        <Button
+          variant={computeInputTypeButtonVariant(NaturalLanguageInputTypes.Text)}
+          onClick={() => setInputType(NaturalLanguageInputTypes.Text)}
+        >
+          By text
+        </Button>{" "}
+        or{" "}
+        <Button
+          variant={computeInputTypeButtonVariant(NaturalLanguageInputTypes.Voice)}
+          onClick={() => setInputType(NaturalLanguageInputTypes.Voice)}
+        >
+          By voice
+        </Button>
+      </div>
+      <button onClick={() => callback("SELECT * FROM test;")}>Generate formal query</button>
+    </>
+  );
+
+  function computeInputTypeButtonVariant(buttonType: NaturalLanguageInputTypes) {
+    return inputType === buttonType ? "contained" : "outlined";
+  }
+}
+
+function FormalQueryView({ query, callback }: { query: string; callback: (queryResult: string) => void }) {
+  return (
+    <>
+      <Title>Your query is:</Title>
+      <textarea>{query}</textarea>
+      <button onClick={() => callback("TestValue1, TestValue2")}>Send</button>
+    </>
+  );
+}
+
+function QueryResultView({ result }: { result: string }) {
+  return (
+    <>
+      <Title>Result:</Title>
+      <textarea readOnly>{result}</textarea>
+    </>
+  );
+}
+
+enum States {
+  NaturalLanguageQuery,
+  LoadFormalQuery,
+  FormalQuery,
+  LoadQueryResult,
+  QueryResult,
+  Error,
+}
+
+enum NaturalLanguageInputTypes {
+  Text,
+  Voice,
 }
 
 const Main = styled("div")({
